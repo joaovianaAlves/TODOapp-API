f6741e50c0ce6863d3b350fce12b6acfdfa32c2f
joaovianaAlves <joaovitorvianaalves@gmail.com>
2023-05-02 20:33:01 -0300

    .

--- routes/routes.js
+++ routes/routes.js
@@ -111,6 +111,17 @@ router.patch('/update/:id', async (req, res) => {
     }
    })
    
+//Autenticacao
+var jwt = require('jsonwebtoken');
+router.post('/login', (req, res, next) => {
+ if (req.body.nome === 'branqs' && req.body.senha === '1234') {
+ const token = jwt.sign({ id: req.body.nome }, 'segredo', { expiresIn: 300 });
+ return res.json({ auth: true, token: token });
+ }
+ res.status(500).json({ message: 'Login invalido!' });
+})
+
+//Nova forma de Autorizacao
 function verificaJWT(req, res, next) {
     const token = req.headers['id-token'];
     if (!token) return res.status(401).json({
@@ -122,3 +133,4 @@ function verificaJWT(req, res, next) {
     });
    }
    
+    
