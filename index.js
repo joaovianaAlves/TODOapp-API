16a23b7eb17f6c74babf19af58639cd5b1d172b3
joaovianaAlves <joaovitorvianaalves@gmail.com>
2023-05-02 20:00:59 -0300

    .

--- index.js
+++ index.js
@@ -35,3 +35,14 @@ db.on('error', (error) => {
 db.once('connected', () => {
  console.log('Database Connected');
 })
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, id-token"
+    );
+    next();
+   });
+   
--- routes/routes.js
+++ routes/routes.js
@@ -25,7 +25,7 @@ router.post('/post', async (req, res) => {
    })
 
    
-router.get('/getAll', async (req, res) => {
+router.get('/getAll', verificaJWT, async (req, res) => {
     try {
     const resultados = await modeloTarefa.find();
     res.json(resultados)
@@ -110,4 +110,25 @@ router.patch('/update/:id', async (req, res) => {
     res.status(400).json({ message: error.message })
     }
    })
+   
+   //Autenticacao
+var jwt = require('jsonwebtoken');
+router.post('/login', (req, res, next) => {
+ if (req.body.nome === 'branqs' && req.body.senha === '1234') {
+ const token = jwt.sign({ id: req.body.nome }, 'segredo', { expiresIn: 300 });
+ return res.json({ auth: true, token: token });
+ }
+ res.status(500).json({ message: 'Login invalido!' });
+})
+
+function verificaJWT(req, res, next) {
+    const token = req.headers['id-token'];
+    if (!token) return res.status(401).json({
+    auth: false, message: 'Token nao fornecido'
+    });
+    jwt.verify(token,'segredo', function (err, decoded) {
+    if (err) return res.status(500).json({ auth: false, message: 'Falha !' });
+    next();
+    });
+   }
    
