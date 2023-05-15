88a4b1dd64b89705209b542076180dd8bdb700ed
joaovianaAlves <joaovitorvianaalves@gmail.com>
2023-05-15 13:10:05 -0300

    .

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router()
 module.exports = router;
 const modeloTarefa = require('../models/tarefa');
+const userModel = require('../models/user')
 
 const backupid = "64262567b6b3a1d705294dac";
 
@@ -24,6 +25,20 @@ router.post('/post', verificaJWT, async (req, res) => {
     }
    })
 
+router.post('/postUser', verificaJWT, async (req, res) => {
+    const objetoUser = new userModel({
+    username: req.body.username,
+    password: req.body.password,
+    isAdmin: req.body.isAdmin
+    })
+    try {
+    const userSalvo = await objetoUser.save();
+    res.status(200).json(userSalvo)
+    }
+    catch (error) {
+    res.status(400).json({ message: error.message })
+    }
+   })
    
 router.get('/getAll', verificaJWT, async (req, res) => {
     try {
@@ -50,6 +65,17 @@ router.delete('/delete/:id',verificaJWT,async (req, res) => {
     try {
         const id = req.params.id;
         const resultado = await modeloTarefa.findByIdAndDelete(req.params.id)
+        res.json(resultado)
+    }
+    catch (error) {
+    res.status(400).json({ message: error.message })
+    }
+   })
+
+   router.delete('/deleteUser/:id',verificaJWT,async (req, res) => {
+    try {
+        const id = req.params.id;
+        const resultado = await userModel.findByIdAndDelete(req.params.id)
         res.json(resultado)
     }
     catch (error) {
@@ -96,7 +122,7 @@ router.patch('/undo', verificaJWT, async (req, res) =>{
         }
 })
    
-router.patch('/update/:id',async (req, res) => {
+router.patch('/update/:id', verificaJWT, async (req, res) => {
     try {
     const id = req.params.id;
     const novaTarefa = req.body;
@@ -105,32 +131,6 @@ router.patch('/update/:id',async (req, res) => {
     id, novaTarefa, options
     )
     res.json(result)
-    }
-    catch (error) {
-    res.status(400).json({ message: error.message })
-    }
-   })
-
-   router.post('/postUser', verificaJWT, async (req, res) => {
-    const objetoUser = new userModel({
-    username: req.body.username,
-    password: req.body.password,
-    admLogado: req.body.admLogado
-    })
-    try {
-    const userSalvo = await objetoUser.save();
-    res.status(200).json(userSalvo)
-    }
-    catch (error) {
-    res.status(400).json({ message: error.message })
-    }
-   })
-
-   router.delete('/deleteUser/:id',verificaJWT,async (req, res) => {
-    try {
-        const id = req.params.id;
-        const resultado = await userModel.findByIdAndDelete(req.params.id)
-        res.json(resultado)
     }
     catch (error) {
     res.status(400).json({ message: error.message })
@@ -152,16 +152,6 @@ router.patch('/update/:id',async (req, res) => {
     }
    })
    
-   router.get('/getAllUsers', verificaJWT, async (req, res) => {
-    try {
-        const resultados = await userModel.find();
-        res.json(resultados)
-        }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-
    //Autenticacao
    var jwt = require('jsonwebtoken');
    router.post('/login', async (req, res) => {
@@ -171,13 +161,23 @@ router.patch('/update/:id',async (req, res) => {
     if (data != null && data.password === req.body.password) {
     const token = jwt.sign({ id: req.body.user }, 'segredo',
     { expiresIn: 300 });
-    return res.json({ token: token , admLogado: data.admLogado});
+    return res.json({ token: token , isAdmin: data.isAdmin});
     }
    
     res.status(500).json({ message: 'Login invalido!' });
     } catch (error) {
     res.status(500).json({ message: error.message })
 }
+})
+
+router.get('/getAllUsers', verificaJWT, async (req, res) => {
+    try {
+        const resultados = await userModel.find();
+        res.json(resultados)
+        }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 })
 
 //Nova forma de Autorizacao
@@ -191,5 +191,3 @@ function verificaJWT(req, res, next) {
     next();
     });
    }
-   
-    
